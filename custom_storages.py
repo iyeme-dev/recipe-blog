95e15a61573796292b3312cca049bbe3c142f0e6
iyeme-dev <iyemeaji@yahoo.com>
2026-01-22 15:41:19 +0000

    chore(storage): harden S3 storage classes

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = getattr(settings, "STATICFILES_LOCATION", "static")
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = getattr(settings, "MEDIAFILES_LOCATION", "media")
+    file_overwrite = False
