2a2f148ba95ddf634516686615a58b993af122a0
iyeme-dev <iyemeaji@yahoo.com>
2026-01-22 04:23:40 +0000

    add EditRecipe view with owner-only permission and update views and url.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     AddRecipe,
     Recipes,
-    RecipeDetail, DeleteRecipe
+    RecipeDetail, DeleteRecipe,
+    EditRecipe
 )
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path("", Recipes.as_view(), name="recipes"),
     path("<slug:pk>/", RecipeDetail.as_view(), name="recipe_detail"),
     path("delete/<slug:pk>/", DeleteRecipe.as_view(), name="delete_recipe"),
+    path("edit/<slug:pk>/", EditRecipe.as_view(), name="edit_recipe",)
 ]
